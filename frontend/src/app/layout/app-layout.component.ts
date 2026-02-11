a2675f58763337b5267d7ce6af8d95f76970c213
vazrala vinith reddy <vvrva@MacBook-Pro.local>
2026-02-10 23:12:04 -0500

    made navbar vertical

--- frontend/src/app/layout/app-layout.component.ts
+++ frontend/src/app/layout/app-layout.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../core/auth/auth.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [MatButtonModule, MatIconModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
 })
